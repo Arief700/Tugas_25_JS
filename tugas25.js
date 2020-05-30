e3c47f5b5ef4f442f6cdc3c3a6f4db7d55fc2ab7
Arief Setiawan <59144049+Arief700@users.noreply.github.com>
2020-05-31 01:27:17 +0700

    Update tugas25.js

--- tugas25.js
+++ tugas25.js
@@ -1,4 +1,4 @@
-var myNumber = [2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]
+var myNumber = [2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16];
 console.log("Sebelumnya : " + myNumber);
 
 var mySort = myNumber.sort()
